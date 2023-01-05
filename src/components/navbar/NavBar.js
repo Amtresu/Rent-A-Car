eba6ff428317b0422b0307c0f6c3702d35d5df85
David-Kasilia <davidkasilia001@gmail.com>
2023-01-05 21:10:24 +0300

    Create style for active link

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -11,26 +11,36 @@ const NavBar = () => {
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-dark min-vh-100">
                     <img className="logo" src={Logo} alt="Logo rental call" />
+                    <div class="container-fluid nav-link">
+                        <NavLink to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline ms-4">CARS</span>
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline nav-link ">CARS</span>
-                    </NavLink>
+                    <div class="container-fluid nav-link">
+                        <NavLink to="/reserve" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline ms-4">RESERVE</span>
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/reserve" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline nav-link ">RESERVE</span>
-                    </NavLink>
+                    <div class="container-fluid nav-link">
+                        <NavLink to="/reservations" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline ms-4">MY RESERVATIONS</span>
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/reservations" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline nav-link ">MY RESERVATIONS</span>
-                    </NavLink>
+                    <div class="container-fluid nav-link">
+                        <NavLink to="/add_car" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline ms-4">ADD CAR</span>
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/add_car" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline nav-link">ADD CAR</span>
-                    </NavLink>
+                    <div class="container-fluid nav-link">
+                        <NavLink to="/delete_car" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
+                            <span class="fs-5 d-none d-sm-inline ms-4">DELETE CAR</span>
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/delete_car" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-dark  text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline nav-link">DELETE CAR</span>
-                    </NavLink>
                 </div>
 
             </div>
