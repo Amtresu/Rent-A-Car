f91600bd8aa533e10ff9ccae4de0242616f7034e
Raga <ragawilliam570@gmail.com>
2023-01-09 17:37:44 +0300

    Add login logic

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,5 @@
-/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+// import axios from 'axios';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -9,6 +10,7 @@ function Login() {
     email: '',
     password: '',
   });
+
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -18,10 +20,43 @@ function Login() {
     });
   };
 
-  console.log(state);
+  // const LOGIN_API = 'http://127.0.0.1:3000/login';
+  // const LOGIN_API = 'https://final-capstone-api.onrender.com/login';
+
+  // const handleUserLogIn = async (obj) => {
+  //   const response = await fetch('http://127.0.0.1:3000/login', {
+  //     method: 'POST',
+  //     header: {
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //     body: JSON.stringify({ user: obj }),
+  //   });
+  //   console.log('William in Login: ', response);
+  //   const data = await response.json();
+  //   console.log('William in login: ', data);
+  //   return data;
+  // };
+
+  // const handleUserLogIn = async (obj) => {
+  //   axios.post(
+  //     'http://127.0.0.1:3000/login',
+  //     {
+  //       user: obj,
+  //     },
+  //   ).then((response) => {
+  //     console.log(response);
+  //     const data = response.json();
+  //     console.log(data);
+  //     return data;
+  //   }).catch((error) => {
+  //     const err = error;
+  //     return err;
+  //   });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // handleUserLogIn(state);
     dispatch(userLogIn(state));
   };
 
@@ -78,7 +113,7 @@ function Login() {
 
               <div className="text-center text-lg-start mt-4 pt-2">
                 <button
-                  type="button"
+                  type="submit"
                   className="btn btn-primary"
                 >
                   Login
