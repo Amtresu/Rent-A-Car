25d7a30aa8259e1e3e427ccd18a4c116ca525598
David-Kasilia <davidkasilia001@gmail.com>
2023-01-10 11:49:22 +0300

    Fix errors and linters

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,12 +6,10 @@ import {
   applyMiddleware,
 } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import carReducer from './cars/car';
 import usersReducer from './users/users';
 import carsReducer from './cars/cars';
 
 const rootReducer = combineReducers({
-  car: carReducer,
   user: usersReducer,
   cars: carsReducer,
 });
