4834929f6a91813da04f1c8472d98753fa4946f7
PRATAP-KUMAR <pratap@fastmail.fm>
2023-01-10 16:58:03 +0530

    Add reserve-car to store

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -1,8 +1,8 @@
 /* eslint-disable default-param-last */
 import axios from 'axios';
 
-const reserveCarAPI = 'https://final-capstone-api.onrender.com/reservations';
-// const reserveCarAPI = 'http://localhost:3000/reservations';
+// const reserveCarAPI = 'https://final-capstone-api.onrender.com/reservations';
+const reserveCarAPI = 'http://localhost:3000/reservations';
 
 // Actions
 const RESERVE_CAR = 'RESERVE_CAR';
@@ -19,26 +19,18 @@ export const reserveCar = (payload) => ({
 
 // Fetch Cars
 export const reserveCarAsync = (obj) => async (dispatch) => {
-    console.log("Obj ", obj)
+    // console.log("Obj ", obj)
     const obj_two = {
         reserve_date: obj.reserve_date.slice(0, 10),
         car_id: +obj.car_id,
         user_id: obj.user_id
       }
-    console.log("object two ", obj_two)
+    // console.log("object two ", obj_two)
   const response = await axios.post(reserveCarAPI, obj_two);
   const data = await response.data;
-  dispatch(fetchCars(data));
+  console.log("Data: ", data);
+  dispatch(reserveCar(data));
 };
-
-axios({
-    method: 'post',
-    url: '/login',
-    data: {
-      firstName: 'Finn',
-      lastName: 'Williams'
-    }
-  });
 
 // Reducer
 const reservationReducer = (state = initialState, action) => {
