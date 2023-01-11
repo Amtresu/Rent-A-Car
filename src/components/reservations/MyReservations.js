c587ee710b39b162841329da04c4fa40252cc039
David-Kasilia <davidkasilia001@gmail.com>
2023-01-12 00:49:39 +0300

    Completed reservations llist

--- src/components/reservations/MyReservations.js
+++ src/components/reservations/MyReservations.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 // import { Link } from 'react-router-dom';
-import { fetchReservationsAsync } from '../../redux/reservations/reservations';
+import { fetchReservationsAsync } from '../../redux/reservations/myReservations';
 
 function MyReservations() {
-  const { reservations } = useSelector((state) => state.reserve);
+  const { reservations } = useSelector((state) => state.reserved);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,36 +15,27 @@ function MyReservations() {
 
   return (
     <div className="reservations">
-      <h2 className="fw-bold home-text ms-5"> MY RESERVATIONS</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
+      <h2 className="fw-bold  ms-5"> MY RESERVATIONS</h2>
+      {reservations.map((reserved) => (
+        <table className="table table-striped-columns" key={reserved.id}>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Car Name</th>
+              <th scope="col">Car Model</th>
+              <th scope="col">Car Reservation Day</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>{reserved.car.name}</td>
+              <td>{reserved.car.model}</td>
+              <td>{reserved.reservation.reserve_date}</td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,11 +9,13 @@ import thunk from 'redux-thunk';
 import usersReducer from './users/users';
 import carsReducer from './cars/cars';
 import reservationReducer from './reservations/reservations';
+import myReservationsReducer from './reservations/myReservations';
 
 const rootReducer = combineReducers({
   user: usersReducer,
   cars: carsReducer,
   reserve: reservationReducer,
+  reserved: myReservationsReducer,
 });
 
 const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
