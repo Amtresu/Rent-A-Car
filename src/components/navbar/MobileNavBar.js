536b10044b4cbfd9960f883a5050b848cc18030f
David-Kasilia <davidkasilia001@gmail.com>
2023-01-13 00:41:39 +0300

    Completed styling the Navbar for mobile

--- src/components/navbar/MobileNavBar.js
+++ src/components/navbar/MobileNavBar.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import Logo from '../../assets/logo.png';
+import {
+  FaTwitter, FaVimeoV, FaPinterest, FaFacebook, FaGooglePlus,
+} from 'react-icons/fa';
+import Logo from '../../assets/logo.png';
 
 function MobileNavBar() {
   return (
     <div className="mobile-nav-bar">
       <nav className="navbar bg-light fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Offcanvas navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+              <h5 className="offcanvas-title fw-semibold" id="offcanvasNavbarLabel">WELCOME</h5>
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
             </div>
             <div className="offcanvas-body text-dark">
               <div className="offcanvas-body">
-                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <img className="logo" src={Logo} alt="Logo rental call" />
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 mobile-items">
                   <li className="nav-item">
                     <NavLink to="/" className="nav-link text-decoration-none">
                       <span className="fs-5 fw-bold">CARS</span>
@@ -46,6 +49,30 @@ function MobileNavBar() {
                   </li>
                 </ul>
 
+              </div>
+              <div className="container-fluid mb-5 mobile-footer">
+                <div className="d-flex justify-content-between me-5 ms-5">
+                  <a href="/socials" className="list-group-item list-group-item-action">
+                    <FaTwitter className="fa-brands" />
+                  </a>
+                  <a href="/socials" className="list-group-item list-group-item-action">
+                    <FaFacebook className="fa-brands" />
+                  </a>
+                  <a href="/socials" className="list-group-item list-group-item-action">
+                    <FaGooglePlus className="fa-brands" />
+                  </a>
+                  <a href="/socials" className="list-group-item list-group-item-action">
+                    <FaVimeoV className="fa-brands" />
+                  </a>
+                  <a href="/socials" className="list-group-item list-group-item-action">
+                    <FaPinterest className="fa-brands" />
+                  </a>
+                </div>
+                <div className="container mt-3 ms-3">
+                  <h6 className="fa-text fw-bold">
+                    &copy; 2015 PIAGGIO & C.S.P.A - PIVA
+                  </h6>
+                </div>
               </div>
             </div>
           </div>
