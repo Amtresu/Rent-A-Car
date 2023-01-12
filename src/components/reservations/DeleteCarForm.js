29c96af6282d5e4eaf8c067046625a7fda1e0fff
Conor <conor.follette@gmail.com>
2023-01-12 16:35:11 +0900

    Added basic delete functionality

--- src/components/reservations/DeleteCarForm.js
+++ src/components/reservations/DeleteCarForm.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCarsAsync, deleteCars } from '../../redux/cars/cars';
 
 function DeleteCarForm() {
+  const { cars } = useSelector((state) => state.cars);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCarsAsync());
+  }, []);
   return (
     <div className="reservations">
       <h2 className="fw-bold home-text ms-5"> DELETE A CAR</h2>
+      {
+         cars.map((car) => (
+
+           <div key={car.id}>
+             <p>{car.name}</p>
+             <button
+               type="submit"
+               onClick={() => dispatch(deleteCars(car.id))}
+             >
+               Delete
+
+             </button>
+           </div>
+         ))
+      }
     </div>
   );
 }
