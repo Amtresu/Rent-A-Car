304686209a8f6cad2e0a2f7d0118ed0ebb4e961b
Raga <ragawilliam570@gmail.com>
2023-01-13 11:51:07 +0300

    Pass props to protected routes

--- src/App.js
+++ src/App.js
@@ -21,22 +21,22 @@ function App() {
     user = userState;
   }
 
-  console.log(user);
-  console.log(user.role.name);
+  // console.log(user);
+  // console.log(user.role.name);
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/login" element={user.authenticated ? <Navigate to="/" /> : <Login />} />
         <Route path="/registrations" element={user.authenticated ? <Navigate to="/" /> : <Registration />} />
-        <Route element={<ProtectedRoutes />}>
+        <Route element={<ProtectedRoutes user={user} />}>
           <Route path="/" element={<Cars />} />
           <Route path="/reserve" element={<ReserveForm />} />
-          <Route path="/reservations" element={<MyReservations />} />
+          <Route path="/reservations" element={<MyReservations user={user} />} />
           <Route path="/cars/:id" element={<CarDetails />} />
         </Route>
         <Route path="/login" element={user.authenticated ? <Navigate to="/" /> : <Login />} />
-        <Route element={<ProtectedAdminRoutes />}>
+        <Route element={<ProtectedAdminRoutes user={user} />}>
           <Route path="/add_car" element={<AddCarForm />} />
           <Route path="/delete_car" element={<DeleteCarForm />} />
         </Route>
