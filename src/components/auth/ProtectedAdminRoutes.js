fe26668db65f402d95d6578905aae14435ba0bdb
Conor <conor.follette@gmail.com>
2023-01-13 09:03:26 +0900

    Created protectedAdminRoutes

---
+++ src/components/auth/ProtectedAdminRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router';
+import Cars from '../cars/Cars';
+import Login from './Login';
+
+const useAuth = () => {
+  const user = useSelector((state) => state.user);
+  if (user?.role?.name === 'admin' && user?.authenticated === true) {
+    return true;
+  }
+  return false;
+};
+
+function ProtectedRoutes() {
+  const user = useSelector((state) => state.user);
+  const isAuth = useAuth();
+  if (user?.authenticated === true) {
+    return isAuth ? <Outlet /> : <Cars />;
+  }
+  return <Login />;
+}
+
+export default ProtectedRoutes;
