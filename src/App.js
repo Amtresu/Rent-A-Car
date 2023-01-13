4ee0ce4cea3664ad40eeed56a74efbf31a5e1b6e
Conor <conor.follette@gmail.com>
2023-01-13 10:33:23 +0900

    fixed linter errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Cars from './components/cars/Cars';
